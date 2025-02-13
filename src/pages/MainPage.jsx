b54c4cc7f3c15cc7af71405fa216e786804cdd1f
akineverused <210103140@stu.sdu.edu.kz>
2025-02-13 21:16:07 +0500

    ne_pomnyu

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import IntroSection from "../Components/UI/IntroSection/IntroSection";
 
 
 const MainPage = () => {
     return (
         <div>
-
+            <IntroSection/>
         </div>
     );
 };
