e45bda743b39440cd527bcdc4ac37f2bf6df69e2
akineverused <210103140@stu.sdu.edu.kz>
2025-02-14 16:38:27 +0500

    Slider_without_customization

--- src/Components/UI/Slider/Slider.jsx
+++ src/Components/UI/Slider/Slider.jsx
@@ -4,7 +4,7 @@ import "./Slider.css"
 import About from "../About/About";
 import  "../About/About.css"
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from 'swiper/modules';
+import {Autoplay, Pagination} from 'swiper/modules';
 import "swiper/css";
 import 'swiper/css/pagination';
 
@@ -12,11 +12,25 @@ import 'swiper/css/pagination';
 const Slider = () => {
     return (
         <div>
-            <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
+            <Swiper
+                pagination={true}
+                modules={[Pagination, Autoplay]}
+                loop={true}
+                autoplay={{ delay: 7000, disableOnInteraction: false }}
+                speed={3000}
+                className="mySwiper">
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <About/>
+                </SwiperSlide>
             </Swiper>
         </div>
     );
