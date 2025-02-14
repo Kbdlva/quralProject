2df531375f8d118e182ea1afe16aa5bd70be8792
akineverused <210103140@stu.sdu.edu.kz>
2025-02-14 13:37:52 +0500

    Slider_another

--- src/Components/UI/Slider/Slider.jsx
+++ src/Components/UI/Slider/Slider.jsx
@@ -3,13 +3,21 @@ import about__img from "../About/about__img.png"
 import "./Slider.css"
 import About from "../About/About";
 import  "../About/About.css"
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from 'swiper/modules';
+import "swiper/css";
+import 'swiper/css/pagination';
 
 
 const Slider = () => {
     return (
         <div>
-
+            <Swiper pagination={true} modules={[Pagination]} className="mySwiper">
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+            </Swiper>
         </div>
     );
 };
